56dde7de051105104fb3e1a9561497e9a6ae1665
Dylan Louichon <dylanlouichon@gmail.com>
2023-11-28 16:19:47 +0100

    Update DataBaseService : fix errors

--- class/Services/DataBaseService.php
+++ class/Services/DataBaseService.php
@@ -12,7 +12,7 @@ class DataBaseService
     const PORT = '3306';
     const DATABASE_NAME = 'carpooling';
     const MYSQL_USER = 'root';
-    const MYSQL_PASSWORD = 'password';
+    const MYSQL_PASSWORD = '';
 
     private $connection;
 
@@ -104,4 +104,184 @@ class DataBaseService
 
         return $isOk;
     }
+
+
+
+    /**
+     * Create an annonce.
+     */
+    public function createAnnonce(string $price, string $startPlace, string $endPlace, DateTime $dateBegining, bool $smoking): bool
+    {
+        $isOk = false;
+
+        $data = [
+            'price' => $price,
+            'startPlace' => $startPlace,
+            'endPlace' => $endPlace,
+            'dateBegining' => $dateBegining->format(DateTime::RFC3339),
+            'smoking' => $smoking
+        ];
+        $sql = 'INSERT INTO annonces (price, startPlace, endPlace, dateBegining, smoking) VALUES (:price, :startPlace, :endPlace, :dateBegining, :smoking)';
+        $query = $this->connection->prepare($sql);
+        $isOk = $query->execute($data);
+
+        return $isOk;
+    }
+
+    /**
+     * Update an annonce.
+     */
+    public function updateAnnonce(string $price, string $startPlace, string $endPlace, DateTime $dateBegining, bool $smoking): bool
+    {
+        $isOk = false;
+
+        $data = [
+            'price' => $price,
+            'startPlace' => $startPlace,
+            'endPlace' => $endPlace,
+            'dateBegining' => $dateBegining->format(DateTime::RFC3339),
+            'smoking' => $smoking
+        ];
+        $sql = 'UPDATE annonces SET price = :price, startPlace = :startPlace, endPlace = :endPlace, dateBegining = :dateBegining, smoking = :smoking WHERE id = :id;';
+        $query = $this->connection->prepare($sql);
+        $isOk = $query->execute($data);
+
+        return $isOk;
+    }
+
+    /**
+     * Delete an annonce.
+     */
+    public function deleteAnnonce(string $id): bool
+    {
+        $isOk = false;
+
+        $data = [
+            'id' => $id,
+        ];
+        $sql = 'DELETE FROM annonces WHERE id = :id;';
+        $query = $this->connection->prepare($sql);
+        $isOk = $query->execute($data);
+
+        return $isOk;
+    }
+
+
+
+    /**
+     * Create a car.
+     */
+    public function createCar(string $brand, string $model, string $year, string $color, string $motorization, int $placesNumber, string $numberplate): bool
+    {
+        $isOk = false;
+
+        $data = [
+            'brand' => $brand,
+            'model' => $model,
+            'yearCar' => $year,
+            'color' => $color,
+            'motorization' => $motorization,
+            'placesNumber' => $placesNumber,
+            'numberplate' => $numberplate
+        ];
+        $sql = 'INSERT INTO cars (brand, model, yearCar, color, motorization, placesNumber, numberplate) VALUES (:brand, :model, :yearCar, :color, :motorization, :placesNumber, :numberplate)';
+        $query = $this->connection->prepare($sql);
+        $isOk = $query->execute($data);
+
+        return $isOk;
+    }
+
+    /**
+     * Update a car.
+     */
+    public function updateCar(string $brand, string $model, string $year, string $color, string $motorization, int $placesNumber, string $numberplate): bool
+    {
+        $isOk = false;
+
+        $data = [
+            'brand' => $brand,
+            'model' => $model,
+            'yearCar' => $year,
+            'color' => $color,
+            'motorization' => $motorization,
+            'placesNumber' => $placesNumber,
+            'numberplate' => $numberplate
+        ];
+        $sql = 'UPDATE cars SET brand = :brand, model = :model, year = :yearCar, color = :color, motorization = :motorization, placesNumber = :placesNumber, numberplate = :numberplate WHERE id = :id;';
+        $query = $this->connection->prepare($sql);
+        $isOk = $query->execute($data);
+
+        return $isOk;
+    }
+
+    /**
+     * Delete a car.
+     */
+    public function deleteCar(string $id): bool
+    {
+        $isOk = false;
+
+        $data = [
+            'id' => $id,
+        ];
+        $sql = 'DELETE FROM cars WHERE id = :id;';
+        $query = $this->connection->prepare($sql);
+        $isOk = $query->execute($data);
+
+        return $isOk;
+    }
+
+
+
+    /**
+     * Create a reservation.
+     */
+    public function createReservation(DateTime $dateTimeReservation): bool
+    {
+        $isOk = false;
+
+        $data = [
+            'dateTimeReservation' => $dateTimeReservation
+        ];
+        $sql = 'INSERT INTO reservations (dateTimeReservation) VALUES (:dateTimeReservation)';
+        $query = $this->connection->prepare($sql);
+        $isOk = $query->execute($data);
+
+        return $isOk;
+    }
+
+    /**
+     * Update a reservation.
+     */
+    public function updateReservation(DateTime $dateTimeReservation): bool
+    {
+        $isOk = false;
+
+        $data = [
+            'dateTimeReservation' => $dateTimeReservation
+        ];
+        $sql = 'UPDATE reservations SET dateTimeReservation = :dateTimeReservation WHERE id = :id;';
+        $query = $this->connection->prepare($sql);
+        $isOk = $query->execute($data);
+
+        return $isOk;
+    }
+
+    /**
+     * Delete a car.
+     */
+    public function deleteReservation(string $id): bool
+    {
+        $isOk = false;
+
+        $data = [
+            'id' => $id,
+        ];
+        $sql = 'DELETE FROM reservations WHERE id = :id;';
+        $query = $this->connection->prepare($sql);
+        $isOk = $query->execute($data);
+
+        return $isOk;
+    }
 }
+
