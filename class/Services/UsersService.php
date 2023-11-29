2fbf9304443cad001ce8d059202d2b7106bfff5b
Dylan Louichon <dylanlouichon@gmail.com>
2023-11-29 15:47:34 +0100

    Update Users Service : Add relations methods

--- class/Services/UsersService.php
+++ class/Services/UsersService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\Entities\Annonce;
+use App\Entities\Car;
 use App\Entities\User;
 use DateTime;
 
@@ -45,6 +47,19 @@ class UsersService
                 if ($date !== false) {
                     $user->setbirthday($date);
                 }
+
+                // Get cars of this user :
+                $cars = $this->getUserCars($userDTO['id']);
+                $user->setCars($cars);
+
+                // Get annonces of this user :
+                $annonces = $this->getUserAnnonces($userDTO['id']);
+                $user->setAnnonces($annonces);
+
+                // Get reservations of this user :
+                $reservations = $this->getUserReservations($userDTO['id']);
+                $user->setReservations($reservations);
+
                 $users[] = $user;
             }
         }
@@ -63,5 +78,129 @@ class UsersService
         $isOk = $dataBaseService->deleteUser($id);
 
         return $isOk;
+    }
+
+
+
+    /**
+     * Create relation bewteen an user and his car.
+     */
+    public function setUserCar(string $userId, string $carId): bool
+    {
+        $isOk = false;
+
+        $dataBaseService = new DataBaseService();
+        $isOk = $dataBaseService->setUserCar($userId, $carId);
+
+        return $isOk;
+    }
+
+    /**
+     * Get cars of given user id.
+     */
+    public function getUserCars(string $userId): array
+    {
+        $userCars = [];
+
+        $dataBaseService = new DataBaseService();
+
+        // Get relation users and cars :
+        $usersCarsDTO = $dataBaseService->getUserCars($userId);
+        if (!empty($usersCarsDTO)) {
+            foreach ($usersCarsDTO as $userCarDTO) {
+                $car = new Car();
+                $car->setId($userCarDTO['id']);
+                $car->setBrand($userCarDTO['brand']);
+                $car->setModel($userCarDTO['model']);
+                $car->setYear($userCarDTO['year']);
+                $car->setColor($userCarDTO['color']);
+                $car->setMotorization($userCarDTO['motorization']);
+                $car->setPlacesNumber($userCarDTO['placesNumber']);
+                $car->setNumberplate($userCarDTO['numberplate']);
+                $userCars[] = $car;
+            }
+        }
+
+        return $userCars;
+    }
+
+
+
+    /**
+     * Create relation bewteen an user and his annonces.
+     */
+    public function setUserAnnonces(string $userId, string $annonceId): bool
+    {
+        $isOk = false;
+
+        $dataBaseService = new DataBaseService();
+        $isOk = $dataBaseService->setUserAnnonces($userId, $annonceId);
+
+        return $isOk;
+    }
+
+    /**
+     * Get annonces of given user id.
+     */
+    public function getUserAnnonces(string $userId): array
+    {
+        $userAnnonces = [];
+
+        $dataBaseService = new DataBaseService();
+
+        // Get relation users and annonces :
+        $usersAnnoncesDTO = $dataBaseService->getUserAnnonces($userId);
+        if (!empty($usersAnnoncesDTO)) {
+            foreach ($usersAnnoncesDTO as $userAnnoncesDTO) {
+                $annonce = new Annonce();
+                $annonce->setId($userAnnoncesDTO['id']);
+                $annonce->setPrice($userAnnoncesDTO['price']);
+                $annonce->setStartPlace($userAnnoncesDTO['startPlace']);
+                $annonce->setEndPlace($userAnnoncesDTO['endPlace']);
+                $annonce->setDateBegining($userAnnoncesDTO['dateBegining']);
+                $annonce->setSmoking($userAnnoncesDTO['smoking']);
+                $userAnnonces[] = $annonce;
+            }
+        }
+
+        return $userAnnonces;
+    }
+
+
+
+    /**
+     * Create relation bewteen an user and his reservations.
+     */
+    public function setUserReservations(string $userId, string $reservationId): bool
+    {
+        $isOk = false;
+
+        $dataBaseService = new DataBaseService();
+        $isOk = $dataBaseService->setUserReservations($userId, $reservationId);
+
+        return $isOk;
+    }
+
+    /**
+     * Get reservations of given user id.
+     */
+    public function getUserReservations(string $userId): array
+    {
+        $userReservations = [];
+
+        $dataBaseService = new DataBaseService();
+
+        // Get relation users and reservations :
+        $usersReservationsDTO = $dataBaseService->getUserReservations($userId);
+        if (!empty($usersReservationsDTO)) {
+            foreach ($usersReservationsDTO as $userReservationsDTO) {
+                $reservation = new Annonce();
+                $reservation->setId($userReservationsDTO['id']);
+                $reservation->setDateBegining($userReservationsDTO['dateTimeReservation']);
+                $userReservations[] = $reservation;
+            }
+        }
+
+        return $userReservations;
     }
 }
