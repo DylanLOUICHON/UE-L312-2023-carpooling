874c0384ae561c45d61266cef750f8f07f36f26d
Dylan Louichon <dylanlouichon@gmail.com>
2023-11-29 16:12:55 +0100

    Update User Controller : Add relations methods

--- class/Controllers/UsersController.php
+++ class/Controllers/UsersController.php
@@ -17,17 +17,44 @@ class UsersController
         if (isset($_POST['firstname']) &&
             isset($_POST['lastname']) &&
             isset($_POST['email']) &&
-            isset($_POST['birthday'])) {
+            isset($_POST['birthday']) &&
+            isset($_POST['cars']) &&
+            isset($_POST['annonces']) &&
+            isset($_POST['reservations'])) {
             // Create the user :
             $usersService = new UsersService();
-            $isOk = $usersService->setUser(
+            $userId = $usersService->setUser(
                 null,
                 $_POST['firstname'],
                 $_POST['lastname'],
                 $_POST['email'],
                 $_POST['birthday']
             );
-            if ($isOk) {
+
+            // Create the user cars relations :
+            $isOk = true;
+            if (!empty($_POST['cars'])) {
+                foreach ($_POST['cars'] as $carId) {
+                    $isOk = $usersService->setUserCar($userId, $carId);
+                }
+            }
+
+            // Create the user annonces relations :
+            if (!empty($_POST['annonces'])) {
+                foreach ($_POST['annonces'] as $annonceId) {
+                    $isOk = $usersService->setUserAnnonce($userId, $annonceId);
+                }
+            }
+
+            // Create the user reservations relations :
+            if (!empty($_POST['reservations'])) {
+                foreach ($_POST['reservations'] as $reservationId) {
+                    $isOk = $usersService->setUserReservation($userId, $reservationId);
+                }
+            }
+
+
+            if ($userId && $isOk) {
                 $html = 'Utilisateur créé avec succès.';
             } else {
                 $html = 'Erreur lors de la création de l\'utilisateur.';
@@ -50,12 +77,36 @@ class UsersController
 
         // Get html :
         foreach ($users as $user) {
+            $carsHtml = '';
+            if (!empty($user->getCars())) {
+                foreach ($user->getCars() as $car) {
+                    $carsHtml .= $car->getBrand() . ' ' . $car->getModel() . ' ' . $car->getYear() . ' ' . $car->getColor() . ' ' . $car->getMotorization() . ' ' . $car->getPlacesNumber() . ' ' . $car->getNumberplate();
+                }
+            }
+
+            $annoncesHtml = '';
+            if (!empty($user->getAnnonces())) {
+                foreach ($user->getAnnonces() as $annonce) {
+                    $annoncesHtml .= $annonce->getPrice() . ' ' . $annonce->getStartPlace() . ' ' . $annonce->getEndPlace() . ' ' . $annonce->getDateBegining() . ' ' . $annonce->getSmoking();
+                }
+            }
+
+            $reservationsHtml = '';
+            if (!empty($user->getReservations())) {
+                foreach ($user->getReservations() as $reservation) {
+                    $reservationsHtml .= $reservation->getDateTimeReservation();
+                }
+            }
+
             $html .=
                 '#' . $user->getId() . ' ' .
                 $user->getFirstname() . ' ' .
                 $user->getLastname() . ' ' .
                 $user->getEmail() . ' ' .
-                $user->getBirthday()->format('d-m-Y') . '<br />';
+                $user->getBirthday()->format('d-m-Y') . ' ' .
+                $carsHtml . ' ' .
+                $annoncesHtml . ' ' .
+                $reservationsHtml . '<br />';
         }
 
         return $html;
